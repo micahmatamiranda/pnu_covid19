64fa8468bd982394df59addb06db83cbadf2bb6f
Micah Mata Miranda <micahmatamiranda@gmail.com>
2020-03-18 14:05:20 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,22 +113,14 @@
     });
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
+  // Advisory carousel (uses the Owl Carousel library)
+  $("#owladvisory").owlCarousel({
+    items: 1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:10000,
+    autoplayHoverPause:true
   });
 
   // Initi AOS
